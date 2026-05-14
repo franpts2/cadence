1c65fb5e94146726c3f05a2ed191cf18d31a8c73
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2026-05-14 22:33:46 +0100

    Integrated Calendar component in website

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Calendar from '$lib/Calendar.svelte';
+</script>
+
+<div class="h-screen w-screen overflow-hidden bg-zinc-950">
+	<Calendar />
+</div>
